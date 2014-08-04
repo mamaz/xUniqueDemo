0d3ef7238e4c1fd3391ecc9c77af4ead9b33f651
Hisma Mulya S <mamaz@icehousecorp.com>
2014-08-04 14:20:41 +0700

    Add fourth view controller

---
+++ XUniqueDemo/XUniqueDemo/MMZFourthViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMZFourthViewController.h
+//  XUniqueDemo
+//
+//  Created by Hisma Mulya S on 8/4/14.
+//  Copyright (c) 2014 mamaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMZFourthViewController : UIViewController
+
+@end
