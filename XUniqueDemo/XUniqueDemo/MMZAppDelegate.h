af46e2ebc5b8f5da8fe8c7d0716618fb88fea319
Hisma Mulya S <mamaz@icehousecorp.com>
2014-08-04 10:36:01 +0700

    Add project to repository

---
+++ XUniqueDemo/XUniqueDemo/MMZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MMZAppDelegate.h
+//  XUniqueDemo
+//
+//  Created by Hisma Mulya S on 8/4/14.
+//  Copyright (c) 2014 mamaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XUniqueDemo/XUniqueDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XUniqueDemo
+//
+//  Created by Hisma Mulya S on 8/4/14.
+//  Copyright (c) 2014 mamaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MMZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MMZAppDelegate class]));
+    }
+}
